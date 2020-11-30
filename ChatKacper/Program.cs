df5d94401666236e814f8719c1ce0c0888da1b51
Michał <m>
2020-11-30 10:02:13 +0100

    Add project files.

---
+++ ChatKacper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ChatKacper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server starting");
+            var server = new Server("127.0.0.1", 14000);
+            server.ClientConnected += Server_ClientConnected;
+
+            Task task = Task.Run(() => server.Start());
+
+            Console.ReadKey();            
+            server.Stop(); // zadamie zatrzymania serwera - w tle serwer sie zatrzymuje
+            Console.WriteLine("Stopping...");
+
+            task.Wait(); // czekaj az to zatrzymanei rzeczewiscie sie zrealizuje
+           
+        }
+
+        private static void Server_ClientConnected(AddressFamily addressFamily)
+        {
+           // addressFamily
+            Console.WriteLine("Client connected");
+        }
+    }
+}
---
+++ ChatKacper/Server.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatKacper
+{
+    public delegate void ClientConnected(AddressFamily addressFamily);
+   // public delegate void ClientConnected2(string arg);
+
+    class Server
+    {
+        private object locker = new object();
+        public Server(string host, int port)
+        {
+            if (string.IsNullOrEmpty(host))
+            {
+                throw new ArgumentException($"'{nameof(host)}' cannot be null or empty", nameof(host));
+            }
+            Host = host;
+            Port = port;
+        }
+
+        public event ClientConnected ClientConnected;
+
+        public string Host { get; }
+        public int Port { get; }
+        public bool Stopped { get => stopped; private set => stopped = value; }
+        public bool Stopping
+        {
+            get
+            {
+                lock (locker)
+                {
+                    return stopping;
+                }
+            }
+
+            private set
+            {
+                lock (locker)
+                {
+                    stopping = value;
+                }
+            }
+        }
+
+        private bool stopping = false;
+        private bool stopped = true;
+
+        public void Start()
+        {
+            Stopped = false;
+            TcpListener _clientsListener = new TcpListener(IPAddress.Parse(Host), Port);
+
+            while (!Stopping)
+            {
+                if (_clientsListener.Pending())
+                {
+                    TcpClient tcpClient = _clientsListener.AcceptTcpClient();                    
+                    ClientConnected.Invoke(tcpClient.Client.AddressFamily);
+                }
+                Thread.Sleep(1000);
+            }
+            Stopped = true;
+
+        }
+        public void Stop()
+        {
+            Stopping = true;
+        }
+    }
+}
